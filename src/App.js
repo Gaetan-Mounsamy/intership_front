f9ebe36d0319e3a4c07002a96a6d37b44c317799
Gaetan Mounsamy <gaetan.mounsamy@efrei.net>
2024-01-22 21:52:48 +0100

    backup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import About from "./pages/about";
-
+import Dashboard from './components/dashboard/Dashboard';
+import Preferences from './components/preferences/Preferences';
 
 function App() {
   return (
-      <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-      </Router>
+      <div className="wrapper">
+          <h1>Application</h1>
+          <Router>
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/preferences" element={<Preferences />} />
+              </Routes>
+          </Router>
+      </div>
   );
 }
 
