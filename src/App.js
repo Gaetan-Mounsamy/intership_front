ce73e54f22fdb385039429bbdafaf307ed6c71a8
Gaetan Mounsamy <gaetan.mounsamy@efrei.net>
2024-01-22 22:51:27 +0100

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Dashboard from './components/dashboard/Dashboard';
 import Preferences from './components/preferences/Preferences';
+import Login from "./components/login/Login";
+import UseToken from './UseToken';
 
+
+function setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+}
 function App() {
-  return (
+
+
+
+    const { token, setToken } = UseToken();
+    if(!token){
+        return <Login setToken={setToken} />
+    }
+    return (
       <div className="wrapper">
           <h1>Application</h1>
           <Router>
